32394a8eabb2c6c8d1b083fe6d3ebe7bc3015257
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-08-23 11:38:54 +0530

    Update logics.py
    Now a particular move on board can checked if it is legal or not

--- logics.py
+++ logics.py
@@ -28,3 +28,33 @@ class board:
             for y in range(coords[1]-2, -1, -1):
                 if Board[coords[0]][y] == color:
                     return True
+                
+        if Board[max(coords[0]+1, 7)][max(coords[1]+1, 7)] == -color:
+            for i in range(2,8):
+                if max(coords[0], coords[1]) + i > 7:
+                    break
+                if Board[coords[0]+i][coords[1]+i] == color:
+                    return True
+        
+        if Board[min(coords[0]-1, 0)][max(coords[1]+1, 7)] == -color:
+            for i in range(2,8):
+                if coords[0]-i < 0 or coords[1]+i > 7:
+                    break
+                if Board[coords[0]-i][coords[1]+i] == color:
+                    return True
+        
+        if Board[max(coords[0]+1, 7)][min(coords[1]-1, 0)] == -color:
+            for i in range(2,8):
+                if coords[0]+i > 7 or coords[1]-i < 0:
+                    break
+                if Board[coords[0]+i][coords[1]-i] == color:
+                    return True
+        
+        if Board[min(coords[0]-1, 0)][min(coords[1]-1, 0)] == -color:
+            for i in range(2,8):
+                if min(coords[0], coords[1]) - i < 0:
+                    break
+                if Board[coords[0]-i][coords[1]-i] == color:
+                    return True
+        
+        return False
