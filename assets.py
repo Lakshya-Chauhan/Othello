74c090fd2a2bc46fedbb335051b664c0254a9937
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-08-23 14:32:54 +0530

    Add files via upload

---
+++ assets.py
@@ -0,0 +1,5 @@
+import pygame
+
+woodBG = pygame.image.load("assets/images/woodBG.jpg")
+whiteD = pygame.image.load("assets/images/whiteDisc.png")
+blackD = pygame.image.load("assets/images/blackDisc.png")
