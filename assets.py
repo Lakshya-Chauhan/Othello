6a4838d10a0f07a23b8f212d718fa0e9139959df
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-09-28 20:21:22 +0530

    Update assets.py

--- assets.py
+++ assets.py
@@ -3,3 +3,5 @@ import pygame
 woodBG = pygame.image.load("assets/images/woodBG.jpg")
 whiteD = pygame.image.load("assets/images/whiteDisc.png")
 blackD = pygame.image.load("assets/images/blackDisc.png")
+
+DISC = [whiteD, blackD]
